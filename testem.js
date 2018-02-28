4dd84d7aa6a2a1824814d6eab6d8d3ddc672cc0d
Sam Selikoff <sam.selikoff@gmail.com>
2018-02-28 14:47:06 -0500

    Update testem

--- testem.js
+++ testem.js
@@ -5,17 +5,19 @@ module.exports = {
     'Chrome'
   ],
   launch_in_dev: [
-    'Chrome'
   ],
   browser_args: {
     Chrome: {
       mode: 'ci',
       args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
-      ]
+      ].filter(Boolean)
     }
   }
 };
