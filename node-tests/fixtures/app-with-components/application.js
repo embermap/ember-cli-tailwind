20c96e7ab511d0d842bd25744239a2368fb49434
Sam Selikoff <sam.selikoff@gmail.com>
2018-06-12 15:31:10 -0400

    Fix imports in modules.css file and blueprint

---
+++ node-tests/fixtures/app-with-components/application.js
@@ -0,0 +1,36 @@
+// const fs = require('fs');
+const fixturify = require('fixturify')
+const path = require('path');
+// const ADDON_ROOT = path.resolve(__dirname, '..', '..', '..');
+
+module.exports = function() {
+  return {
+    application: {
+      app: {
+        styles: {
+          'app.css': ''
+        },
+        tailwind: fixturify.readSync(path.join(__dirname, './tailwind')),
+        'index.html': ''
+      },
+      config: {
+        'environment.js': "module.exports = function() { return { modulePrefix: 'application' } };"
+      },
+      'ember-cli-build.js': 'module.exports = function() { return { "ember-cli-tailwind": { buildTarget: "dummy" } } };',
+      'package.json': JSON.stringify({
+        name: 'application',
+        devDependencies: {
+          'ember-cli': '*',
+          'ember-cli-babel': '*',
+          'ember-cli-htmlbars': '*',
+          'ember-engines': '*',
+          'ember-source': '*',
+          'loader.js': '*'
+        },
+        'ember-addon': {
+          paths: ['../ember-cli-tailwind']
+        }
+      }),
+    }
+  }
+};
---
+++ node-tests/fixtures/app-with-components/tailwind/config/tailwind.js
@@ -0,0 +1,11 @@
+export default {
+  textColors: {
+    'ember-red': '#E04E39'
+  },
+
+  options: {
+    prefix: '',
+    important: false,
+    separator: ':'
+  }
+};
