b6a00d1800030baa1873e1539a25d758a9d3dd74
Tammy Soo <tammy@getfeedback.com>
2018-06-28 10:07:19 -0700

    removing the intializer error affecting load in flabongo

--- app/instance-initializers/ember-cli-tailwind.js
+++ app/instance-initializers/ember-cli-tailwind.js
@@ -13,6 +13,11 @@ const TailwindApplicationRoute = Route.extend({
 });
 
 export function initialize(appInstance) {
+  if (!appInstance.lookup) {
+    // not compatible with ember 1.13
+    return;
+  } 
+
   let fastboot = appInstance.lookup('service:fastboot');
   let fastbootIsInstalled = fastboot;
   let fastbootIsNotInstalled = !fastboot;
