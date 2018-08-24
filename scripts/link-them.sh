1028dd9ab390bb8136ecad4c0318fe0ea8446ae0
Sam Selikoff <sam.selikoff@gmail.com>
2018-08-24 12:03:40 -0400

    workin

--- scripts/link-them.sh
+++ scripts/link-them.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
- # Copied from https://github.com/ef4/ember-auto-import/blob/master/scripts/link-them.sh
- set -e
- # All packages get a node_modules directory and a .bin link
-for package in "sample-addon" "sample-app"; do
+# Copied from https://github.com/ef4/ember-auto-import/blob/master/scripts/link-them.sh
+set -e
+
+# All packages get a node_modules directory and a .bin link
+for package in "sample-app" "sample-addon"; do
     mkdir -p ./test-projects/$package/node_modules
     pushd ./test-projects/$package/node_modules > /dev/null
     rm -rf .bin
@@ -10,10 +11,18 @@ for package in "sample-addon" "sample-app"; do
     popd > /dev/null
 done
 
- # These packages get to depend on ember-cli-tailwind
+# These packages get to depend on ember-cli-tailwind
 for package in "sample-addon"; do
     pushd ./test-projects/$package/node_modules > /dev/null
     rm -rf ./ember-cli-tailwind
     ln -s ../../.. ./ember-cli-tailwind
+    popd > /dev/null
+done
+
+# These packages get to depend on our sample-addon
+for package in "sample-app"; do
+    pushd ./test-projects/$package/node_modules > /dev/null
+    rm -rf ./sample-addon
+    ln -s ../../sample-addon ./sample-addon
     popd > /dev/null
 done
