c56b8d9457f7a0a3a8486fb999f7328569c46d28
Sam Selikoff <sam.selikoff@gmail.com>
2018-08-24 14:59:42 -0400

    more things

--- scripts/link-them.sh
+++ scripts/link-them.sh
@@ -26,3 +26,29 @@ for package in "sample-app"; do
     ln -s ../../sample-addon ./sample-addon
     popd > /dev/null
 done
+
+# Scenario 2: Disabled styleguide
+# All packages get a node_modules directory and a .bin link
+for package in "sample-addon" "sample-addon-with-tailwind"; do
+    mkdir -p ./test-projects/scenario-2-disabled-styleguide/$package/node_modules
+    pushd ./test-projects/scenario-2-disabled-styleguide/$package/node_modules > /dev/null
+    rm -rf .bin
+    ln -s ../../../../node_modules/.bin .bin
+    popd > /dev/null
+done
+
+# These packages get to depend on ember-cli-tailwind
+for package in "sample-addon-with-tailwind"; do
+    pushd ./test-projects/scenario-2-disabled-styleguide/$package/node_modules > /dev/null
+    rm -rf ./ember-cli-tailwind
+    ln -s ../../../.. ./ember-cli-tailwind
+    popd > /dev/null
+done
+
+# These packages get to depend on sample-addon-with-tailwind
+for package in "sample-addon"; do
+    pushd ./test-projects/scenario-2-disabled-styleguide/$package/node_modules > /dev/null
+    rm -rf ./sample-addon-with-tailwind
+    ln -s ../../../sample-addon-with-tailwind ./sample-addon-with-tailwind
+    popd > /dev/null
+done
